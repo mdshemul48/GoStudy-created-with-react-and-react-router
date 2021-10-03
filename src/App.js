e2c4ba6c39fb96b8a1c703732abbe689a167bdef
MD Shimul <mdshemul480@gmail.com>
2021-10-03 23:28:27 +0600

    setting router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+// all components
+import Home from "./components/Home/Home";
+import AboutUs from "./components/AboutUs/AboutUs";
+import ContactUs from "./components/ContactUs/ContactUs";
+import Products from "./components/Product/Products";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
-    <div className='App'>
-      <h1>hello world</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/about'>
+          <AboutUs />
+        </Route>
+        <Route exact path='/contact'>
+          <ContactUs />
+        </Route>
+        <Route exact path='/products'>
+          <Products />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
