e909e19ec8caf94268edf789503cdd1d4d586d7b
MD Shimul <mdshemul480@gmail.com>
2021-10-04 00:18:44 +0600

    adding text area to header

---
+++ src/components/Home/HeroArea.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+import "./HeroArea.css";
+const HeroArea = () => {
+  return (
+    <Container>
+      <Row>
+        <Col lg={6}>
+          <h1 className='hero-area-title'>
+            Build Skills with <br />
+            Online Courses from <br />
+            expert instructor
+          </h1>
+          <p className='hero-area-text'>
+            Start streaming on-demand video lectures today from top level
+            instructors Attention heatmaps.
+          </p>
+        </Col>
+        <Col lg={6}>pp</Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default HeroArea;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import HeroArea from "./HeroArea";
 
+import "./Home.css";
 const Home = () => {
   return (
     <>
-      <header>
-        <h1>Home</h1>
+      <header className='hero-area'>
+        <HeroArea />
       </header>
     </>
   );
