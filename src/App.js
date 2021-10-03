6c2543fac8f766164928802c3f038501c3d49165
MD Shimul <mdshemul480@gmail.com>
2021-10-04 00:02:23 +0600

    adding not found component

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ import Home from "./components/Home/Home";
 import AboutUs from "./components/AboutUs/AboutUs";
 import ContactUs from "./components/ContactUs/ContactUs";
 import Products from "./components/Product/Products";
+import NotFound from "./components/NotFound/NotFound";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
     <Router>
@@ -26,6 +28,9 @@ function App() {
         </Route>
         <Route exact path='/about'>
           <AboutUs />
+        </Route>
+        <Route path='*'>
+          <NotFound />
         </Route>
       </Switch>
     </Router>
