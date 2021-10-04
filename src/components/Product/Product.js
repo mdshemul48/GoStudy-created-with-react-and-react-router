2d59ee71cfac8c8fe2b4ab4689a07b91727575e5
MD Shimul <mdshemul480@gmail.com>
2021-10-04 19:26:55 +0600

    adding new product to fakeData

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,30 +4,37 @@ import { BsFillPersonFill } from "react-icons/bs";
 import { FiBook } from "react-icons/fi";
 import { Card, Col } from "react-bootstrap";
 
-const Product = () => {
+const Product = (props) => {
+  const {
+    authorName,
+    courseName,
+    totalModule,
+    totalEnrolled,
+    price,
+    poster,
+    authorPicture,
+  } = props.product;
+
   return (
     <Col>
       <Card className='p-0'>
-        <Card.Img
-          variant='top'
-          src='https://raistheme.com/wp/gostudy/learnpress/wp-content/uploads/sites/2/2021/09/BL8312-500x300.jpg'
-        />
+        <Card.Img variant='top' src={poster} />
         <Card.Body>
           {/* product detail area */}
           <div className='d-flex align-items-center'>
             <img
-              src='https://raistheme.com/wp/gostudy/wp-content/uploads/2021/03/profile-21-32x32.jpg'
+              src={authorPicture}
               width='32'
               height='32'
               className='rounded-circle'
               alt=''
             />
-            <p className='mt-3 ms-2'>Kevin Perry</p>
+            <p className='mt-3 ms-2'>{authorName}</p>
           </div>
 
           <Card.Title>
             {" "}
-            <h5>Basics of Cooking for Busy People</h5>
+            <h5>{courseName}</h5>
           </Card.Title>
 
           <hr className='w-80 mx-auto' />
@@ -37,16 +44,16 @@ const Product = () => {
             <div className='d-flex'>
               <div className='d-flex align-items-center'>
                 <BsFillPersonFill className='text-danger' />
-                <span className='ms-1'>1</span>
+                <span className='ms-1'>{totalEnrolled}</span>
               </div>
               <div className='d-flex align-items-center ms-2'>
                 <FiBook className='text-danger' />
-                <span className='ms-1'>10</span>
+                <span className='ms-1'>{totalModule}</span>
               </div>
             </div>
 
             <div className='d-flex align-items-center'>
-              <span className='ms-1 fw-bolder'>$20</span>
+              <span className='ms-1 fw-bolder'>${price}</span>
             </div>
           </div>
         </Card.Body>
