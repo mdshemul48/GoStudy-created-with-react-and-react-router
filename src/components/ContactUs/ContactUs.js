408a3bae160fc2117d0d89884114dfdd12d698bf
MD Shimul <mdshemul480@gmail.com>
2021-10-04 20:57:04 +0600

    adding contact page

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,7 +1,62 @@
 import React from "react";
+import {
+  Container,
+  Form,
+  Row,
+  Col,
+  FloatingLabel,
+  Button,
+} from "react-bootstrap";
 
+import ContactImage from "../../assets/contact-us.svg";
+
+import "./ContactUs.css";
 const ContactUs = () => {
-  return <div>Contact us</div>;
+  return (
+    <section className='mx-5 contact-us-page w-75 mx-auto'>
+      <Row className='d-flex align-items-center pb-5 px-5 g-5'>
+        <Col md={5}>
+          <img src={ContactImage} className='img-fluid' alt='' />
+        </Col>
+        <Col md={7}>
+          <h1>Contact With Us</h1>
+          <Form>
+            <Row className='mb-3'>
+              <Form.Group as={Col} controlId='formGridEmail'>
+                <Form.Label>First Name</Form.Label>
+                <Form.Control type='text' placeholder='Enter Your First Name' />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId='formGridPassword'>
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control type='text' placeholder='Enter Your Last name' />
+              </Form.Group>
+            </Row>
+
+            <Form.Group className='mb-3' controlId='formGridAddress1'>
+              <Form.Label>Email</Form.Label>
+              <Form.Control type='email' placeholder='something@some.com' />
+            </Form.Group>
+
+            <Form.Group className='mb-3' controlId='formGridAddress2'>
+              <Form.Label>Your Massage</Form.Label>
+              <FloatingLabel controlId='floatingTextarea2' label='Your Massage'>
+                <Form.Control
+                  as='textarea'
+                  placeholder='Leave a comment here'
+                  style={{ height: "100px" }}
+                />
+              </FloatingLabel>
+            </Form.Group>
+
+            <Button variant='primary' type='submit'>
+              Send
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </section>
+  );
 };
 
 export default ContactUs;
